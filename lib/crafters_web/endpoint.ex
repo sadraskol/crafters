07cde41bf0fcb2b80e8bf71a6d6398a94706fdc2
Thomas Bracher <thchblog@gmail.com>
2018-01-29 22:27:18 +0100

    use gzip by default

--- lib/crafters_web/endpoint.ex
+++ lib/crafters_web/endpoint.ex
@@ -1,12 +1,8 @@
 defmodule CraftersWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :crafters
 
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phoenix.digest
-  # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :crafters, gzip: false,
+    at: "/", from: :crafters, gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
